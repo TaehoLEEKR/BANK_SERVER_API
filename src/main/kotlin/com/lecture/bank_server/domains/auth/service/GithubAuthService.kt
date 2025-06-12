a8e484a3e99c03953c23ef06c170ae4ef19a5a18
LEE DAE HO <80689135+TaehoLEEKR@users.noreply.github.com>
2025-06-12 21:07:56 +0900

    feat : OAuth interface member funtion

--- src/main/kotlin/com/lecture/bank_server/domains/auth/service/GithubAuthService.kt
+++ src/main/kotlin/com/lecture/bank_server/domains/auth/service/GithubAuthService.kt
@@ -1,6 +1,9 @@
 package com.lecture.bank_server.domains.auth.service
 
 import com.lecture.bank_server.config.OAuth2Config
+import com.lecture.bank_server.interfaces.OAuth2TokenResponse
+import com.lecture.bank_server.interfaces.OAuth2UserResponse
+import com.lecture.bank_server.interfaces.OAuthServiceInterface
 import org.springframework.stereotype.Service
 
 private const val key = "github"
@@ -8,5 +11,15 @@ private const val key = "github"
 @Service(key)
 class GithubAuthService(
     private val config : OAuth2Config
-) {
+) : OAuthServiceInterface {
+    override val providerName: String
+        get() = TODO("Not yet implemented")
+
+    override fun getAccessToken(code: String): OAuth2TokenResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserInfo(accessToken: String): OAuth2UserResponse {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/com/lecture/bank_server/domains/auth/service/GoogleAuthService.kt
+++ src/main/kotlin/com/lecture/bank_server/domains/auth/service/GoogleAuthService.kt
@@ -1,6 +1,9 @@
 package com.lecture.bank_server.domains.auth.service
 
 import com.lecture.bank_server.config.OAuth2Config
+import com.lecture.bank_server.interfaces.OAuth2TokenResponse
+import com.lecture.bank_server.interfaces.OAuth2UserResponse
+import com.lecture.bank_server.interfaces.OAuthServiceInterface
 import org.springframework.stereotype.Service
 
 private const val key = "google"
@@ -8,5 +11,17 @@ private const val key = "google"
 @Service(key)
 class GoogleAuthService(
     private val config : OAuth2Config
-) {
+) : OAuthServiceInterface{
+    override val providerName: String
+        get() = TODO("Not yet implemented")
+
+    override fun getAccessToken(code: String): OAuth2TokenResponse {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserInfo(accessToken: String): OAuth2UserResponse {
+        TODO("Not yet implemented")
+    }
+
+
 }
