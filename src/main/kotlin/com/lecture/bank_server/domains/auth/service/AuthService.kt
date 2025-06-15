f06b97f7a10874b822ee89eca9237dbf5a962c15
TaehoLEEKR <679748@naver.com>
2025-06-15 16:22:08 +0900

    feat : jwt AuthService service Embody add

--- src/main/kotlin/com/lecture/bank_server/domains/auth/service/AuthService.kt
+++ src/main/kotlin/com/lecture/bank_server/domains/auth/service/AuthService.kt
@@ -1,4 +1,26 @@
 package com.lecture.bank_server.domains.auth.service
 
-class AuthService {
+import com.lecture.bank_server.common.exception.CustomException
+import com.lecture.bank_server.common.exception.ErrorCode
+import com.lecture.bank_server.common.jwt.jwtProvider
+import com.lecture.bank_server.interfaces.OAuthServiceInterface
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService(
+    private val oAuth2Services : Map<String,OAuthServiceInterface>
+    private val jwtProvider: jwtProvider
+) {
+    fun handleAuth(state: String, code: String) : String{
+        val provider = state.lowercase()
+
+        val callService = oAuth2Services[provider] ?: throw CustomException(ErrorCode.NOT_FOUND_PROVIDER)
+
+        val accessToken = callService.getAccessToken(code)
+
+        val userInfo = callService.getUserInfo(accessToken.accessToken)
+
+        //userInfo
+
+    }
 }
