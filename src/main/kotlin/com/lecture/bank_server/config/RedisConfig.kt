0ef7bafde290ceb4ac635bc2ff7fc6da5e2d3cf2
LEE DAE HO <80689135+TaehoLEEKR@users.noreply.github.com>
2025-06-30 20:29:57 +0900

    chore: redisson init

--- src/main/kotlin/com/lecture/bank_server/config/RedisConfig.kt
+++ src/main/kotlin/com/lecture/bank_server/config/RedisConfig.kt
@@ -3,7 +3,10 @@ package com.lecture.bank_server.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import java.time.Duration
 
 @Configuration
 class RedisConfig {
@@ -11,11 +14,20 @@ class RedisConfig {
     @Bean
     fun redisConnectionFactory(
         @Value("\${database.redis.host}")   host : String,
-        @Value("\${database.redis.port}")   port : String,
-        @Value("\${database.redis.password:${null}}")   password : String,
-        @Value("\${database.redis.database:${0}}")   database : String,
-        @Value("\${database.redis.timeout:${10000}}")   timeout : String,
-        ) : LettuceConnectionFactory {
+        @Value("\${database.redis.port}")   port : Int,
+        @Value("\${database.redis.password:${null}}")   password : String?,
+        @Value("\${database.redis.database:${0}}")   database : Int,
+        @Value("\${database.redis.timeout:${10000}}")   timeout : Long,
+    ) : LettuceConnectionFactory {
+        val config = RedisStandaloneConfiguration(host, port).apply {
+            password?.let { this.setPassword(it) }
+            setDatabase(database)
+        }
 
+        val clientConfig = LettuceClientConfiguration.builder()
+            .commandTimeout(Duration.ofMillis(timeout))
+            .build()
+
+        return LettuceConnectionFactory(config, clientConfig)
     }
 }
