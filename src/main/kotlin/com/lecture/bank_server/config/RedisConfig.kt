2d43c5fefef5d1307e6f49985e6363c39be9d4d3
LEE DAE HO <80689135+TaehoLEEKR@users.noreply.github.com>
2025-06-30 20:27:06 +0900

    chore : redisson config

--- src/main/kotlin/com/lecture/bank_server/config/RedisConfig.kt
+++ src/main/kotlin/com/lecture/bank_server/config/RedisConfig.kt
@@ -1,15 +1,21 @@
-//package com.lecture.bank_server.config
-//
-//import org.springframework.beans.factory.annotation.Value
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//
-//@Configuration
-//class RedisConfig {
-//
-//    @Bean
-//    fun redisConnectionFactory() {
-//        @Value("\${database.redis.host}")  : String,
-//
-//    }
-//}
+package com.lecture.bank_server.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory(
+        @Value("\${database.redis.host}")   host : String,
+        @Value("\${database.redis.port}")   port : String,
+        @Value("\${database.redis.password:${null}}")   password : String,
+        @Value("\${database.redis.database:${0}}")   database : String,
+        @Value("\${database.redis.timeout:${10000}}")   timeout : String,
+        ) : LettuceConnectionFactory {
+
+    }
+}
