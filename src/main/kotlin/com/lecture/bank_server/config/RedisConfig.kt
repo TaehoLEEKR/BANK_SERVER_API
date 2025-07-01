35d8bb173aa0bcf0c7b1a5357853807a9871528b
LEE DAE HO <80689135+TaehoLEEKR@users.noreply.github.com>
2025-07-01 19:53:26 +0900

    chore: redis base redisTemplate

--- src/main/kotlin/com/lecture/bank_server/config/RedisConfig.kt
+++ src/main/kotlin/com/lecture/bank_server/config/RedisConfig.kt
@@ -3,9 +3,11 @@ package com.lecture.bank_server.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
 import java.time.Duration
 
 @Configuration
@@ -29,5 +31,20 @@ class RedisConfig {
             .build()
 
         return LettuceConnectionFactory(config, clientConfig)
+    }
+
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory) : RedisTemplate<String,String>{
+
+        val template = RedisTemplate<String,String>()
+
+        template.connectionFactory = connectionFactory
+        template.keySerializer = template.stringSerializer
+        template.valueSerializer = template.stringSerializer
+        template.hashKeySerializer = template.stringSerializer
+        template.hashValueSerializer = template.stringSerializer
+        template.afterPropertiesSet()
+
+        return template
     }
 }
