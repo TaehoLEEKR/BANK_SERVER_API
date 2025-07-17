c4f134e23102a4a9fa91ce61fa477e594f915805
LEE DAE HO <80689135+TaehoLEEKR@users.noreply.github.com>
2025-07-17 20:58:35 +0900

    feat: bank service controller 구현 완료

--- src/main/kotlin/com/lecture/bank_server/domains/transactions/controller/TransferController.kt
+++ src/main/kotlin/com/lecture/bank_server/domains/transactions/controller/TransferController.kt
@@ -2,9 +2,12 @@ package com.lecture.bank_server.domains.transactions.controller
 
 import com.lecture.bank_server.domains.transactions.domain.DepositResponse
 import com.lecture.bank_server.domains.transactions.domain.TransferResponse
+import com.lecture.bank_server.domains.transactions.model.DepositRequest
+import com.lecture.bank_server.domains.transactions.model.TransferRequest
 import com.lecture.bank_server.domains.transactions.service.TransactionService
 import com.lecture.bank_server.model.dto.Response
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,13 +17,18 @@ class TransferController(
     val transactionService: TransactionService,
 ) {
     @PostMapping("/deposit")
-    fun deposit() : Response<DepositResponse> {
-        return transactionService.deposit()
+    fun deposit(@RequestBody(required = true) request : DepositRequest) : Response<DepositResponse> {
+        return transactionService.deposit(request.toUlid, request.toAccountId, request.value)
     }
 
     @PostMapping("/transfer")
-    fun transfer() : Response<TransferResponse> {
-        return transactionService.transfer()
+    fun transfer(@RequestBody(required = true) request: TransferRequest) : Response<TransferResponse> {
+        return transactionService.transfer(
+            request.fromUlid,
+            request.fromAccountId,
+            request.toAccountId,
+            request.value,
+        )
     }
 
 
