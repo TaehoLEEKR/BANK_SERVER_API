d48b7d52f40332b2b2467bc7fb80a128f6eb6dfb
TaehoLEEKR <679748@naver.com>
2025-08-02 17:43:34 +0900

    feat: kafkaProducer add

---
+++ src/main/kotlin/com/lecture/bank_server/message/kafkaProducer.kt
@@ -0,0 +1,27 @@
+package com.lecture.bank_server.message
+
+import com.lecture.bank_server.common.logging.Logging
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.slf4j.Logger
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+
+@Component
+class kafkaProducer(
+    private val template : KafkaTemplate<String,Any>,
+    private val log: Logger = Logging.getLogger(KafkaProducer::class.java)
+) {
+    fun sendMessage(topic: String, message: String) {
+
+        val future = template.send(topic, message)
+
+        future.whenComplete { result, ex ->
+            if (ex == null) {
+                log.info("Sent message=[$message] with offset=[${result.recordMetadata.offset()}]")
+            } else {
+                log.error("Unable to send message=[$message] due to : ${ex.message}")
+            }
+        }
+    }
+}
